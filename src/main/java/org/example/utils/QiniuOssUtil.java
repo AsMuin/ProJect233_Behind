c67a9efe26ed3c1e8586d164652741314c1aec58
AsMuin <asmuins@foxmail.com>
2024-09-14 00:56:43 +0800

    update: 更改对象存储服务

--- src/main/java/org/example/utils/QiniuOssUtil.java
+++ src/main/java/org/example/utils/QiniuOssUtil.java
@@ -11,14 +11,14 @@ import java.io.InputStream;
 public class QiniuOssUtil {
     public static String upload(InputStream inputStream, String fileName) {
         //构造一个带指定 Region 对象的配置类
-        Configuration cfg = new Configuration(Region.regionAs0());
+        Configuration cfg = new Configuration(Region.region2());
         cfg.resumableUploadAPIVersion = Configuration.ResumableUploadAPIVersion.V2;// 指定分片上传版本
         //...其他参数参考类注释
         UploadManager uploadManager = new UploadManager(cfg);
         //...生成上传凭证，然后准备上传
         String accessKey = "h6e3bEICraF9Nja0McKaYJJ106IdwoCFhz8K0rZV";//七牛云的AK密钥
         String secretKey = "diY06VXVRkgGsqcYHYmxPQcymOeWTQT1l1f67x23";//七牛云的SK密钥
-        String bucket = "asmuin-demo";//七牛云的空间名称
+        String bucket = "asmuin-test";//七牛云的空间名称
         //默认不指定key的情况下，以文件内容的hash值作为文件名
         String key = "project/dev/"+fileName;
         String result = null;
@@ -35,7 +35,7 @@ public class QiniuOssUtil {
 //                System.out.println(putRet.hash);
                 if (response.statusCode == 200) {
                     System.out.println(response);
-                    result = "http://sjk2ohooq.sabkt.gdipper.com/" + key;//http://xxxxxxx.xxx/换成七牛云提供的测试域名或者空间绑定的域名
+                    result = "http://sjrch0dcp.hn-bkt.clouddn.com/" + key;//http://xxxxxxx.xxx/换成七牛云提供的测试域名或者空间绑定的域名
                 }
             } catch (QiniuException ex) {
                 Response r = ex.response;
